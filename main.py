7821676a8d8b3a6ccaec3f8c8beb2223a3f0e50c
k-bar-n <bard.kosty@gmail.com>
2024-04-01 17:58:39 +0300

    commit

--- main.py
+++ main.py
@@ -4,10 +4,33 @@ import sys
 from PyQt6 import QtWidgets, uic
 
 
+k = 0
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi("qt_p_1_d.ui", self)
+
+        self.Button_2.clicked.connect(self.first)
+
+    def first(self):
+        global k
+        print("lol", k)
+        k += 1
+        if k % 2 == 0:
+            self.Button_2.setStyleSheet("""
+                QPushButton {
+                    background-color: red;
+                }
+            """)
+        else:
+            self.Button_2.setStyleSheet("""
+                QPushButton {
+                    background-color: orange;
+                }
+            """)
+
 
 app = QtWidgets.QApplication(sys.argv)
 
